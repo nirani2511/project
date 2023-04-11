e396774623acadc208e3307e15ac0b389399fac4
nirani2511 <129960814+nirani2511@users.noreply.github.com>
2023-04-11 22:26:11 +0530

    Update app.py

--- project/ui/app.py
+++ project/ui/app.py
@@ -4,23 +4,22 @@ import requests
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-SQUARE_SERVICE_URL = "http://square:80/square"
-CUBE_SERVICE_URL = "http://cube:80/cube"
+SQUARE_SERVICE_URL = "http://square:80"
+CUBE_SERVICE_URL = "http://cube:80"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    square = None
+    cube = None
     if request.method == "POST":
         num = int(request.form["num"])
-        operation = request.form["operation"]
-        if operation == "square":
-            result = requests.get(SQUARE_SERVICE_URL, params={"num": num}).text
-        elif operation == "cube":
-            result = requests.get(CUBE_SERVICE_URL, params={"num": num}).text
-        else:
-            result = "Invalid operation"
-        return render_template("index.html", num=num, operation=operation, result=result)
+        if request.form["operation"] == "square":
+            square = requests.get(SQUARE_SERVICE_URL, params={"num": num}).text
+        elif request.form["operation"] == "cube":
+            cube = requests.get(CUBE_SERVICE_URL, params={"num": num}).text
+        return render_template("index.html", num=num, square=square, cube=cube)
     else:
-        return render_template("index.html")
+        return render_template("index.html", square=square, cube=cube)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
