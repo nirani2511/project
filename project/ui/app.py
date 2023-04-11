5590015f4d331f733bad0118a3434e5195cebd6c
nirani2511 <129960814+nirani2511@users.noreply.github.com>
2023-04-11 22:57:11 +0530

    Update app.py

--- project/ui/app.py
+++ project/ui/app.py
@@ -9,18 +9,17 @@ CUBE_SERVICE_URL = "http://cube:80"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    square = None
+    cube = None
     if request.method == "POST":
         num = int(request.form["num"])
-        operation = request.form["operation"]
-        if operation == "square":
-            res = requests.get(SQUARE_SERVICE_URL, params={"num": num})
-            result = res.json()["result"]
-            return render_template("index.html", num=num, square=result)
-        elif operation == "cube":
-            res = requests.get(CUBE_SERVICE_URL, params={"num": num})
-            result = res.json()["result"]
-            return render_template("index.html", num=num, cube=result)
-        else:
-            return "Invalid operation"
+        if request.form["operation"] == "square":
+            square = requests.get(SQUARE_SERVICE_URL, params={"num": num}).text
+        elif request.form["operation"] == "cube":
+            cube = requests.get(CUBE_SERVICE_URL, params={"num": num}).text
+        return render_template("index.html", num=num, square=square, cube=cube)
+    else:
+        return render_template("index.html", square=square, cube=cube)
 
-    return render_template("index.html")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
